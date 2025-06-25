a43deaf19096b3be6997ee84109533eae4e64184
Ngoni  Developer <ngonikatsidzira@icloud.com>
2025-06-25 22:09:52 +0200

    initial commit

---
+++ Project12b/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Ngoni Katsidzira  on 24/6/2025.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ Project12b/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Ngoni Katsidzira  on 24/6/2025.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
